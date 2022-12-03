f0e26dda55dfdc6888167c460973ce0c7aa8dbb8
Seth <sethbarrett01@gmail.com>
2022-12-03 00:33:41 -0500

    Day3 Pt2 done

--- day3/pt2.py
+++ day3/pt2.py
@@ -5,9 +5,16 @@
 
 
 def main():
+    letters = [c for c in list(map(chr,range(ord('a'),ord('z')+1)))]
+    letters.extend([c for c in list(map(chr,range(ord('A'),ord('Z')+1)))])
+    priorities, sumPrioriites = dict(zip(letters, [int(x) for x in range(1, 52+1)])),  0
+
     with open('input.txt') as f:
-        lines = f.readlines() 
-    
+        lines = f.readlines()
+
+    for i in range(0, len(lines), 3):
+        sumPrioriites += priorities[str(list(set(lines[i].replace('\n', ''))&set(lines[i + 1].replace('\n', ''))&set(lines[i + 2].replace('\n', ''))))[2:-2]]
+    print(sumPrioriites)
 
 
 
