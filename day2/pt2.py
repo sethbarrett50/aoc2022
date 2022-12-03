7c70dca18d513caaba7966b0913e1e214bc0a22d
Seth <sethbarrett01@gmail.com>
2022-12-02 21:22:59 -0500

    Day 2 Part 1 done

--- day2/pt2.py
+++ day2/pt2.py
@@ -5,10 +5,26 @@
 
 
 def main():
-    lines = []
+    lines, moves, score = [], [], 0
     with open('input.txt') as f:
         lines = f.readlines() 
     
+    moveScore = {
+        'AY': 2 + 6,
+        'AX': 1 + 3,
+        'AZ': 3 + 0,
+        'BY': 2 + 3,
+        'BX': 1 + 0,
+        'BZ': 3 + 6,
+        'CY': 2 + 0,
+        'CX': 1 + 6,
+        'CZ': 3 + 3
+    }
+
+    for line in lines:
+        moves = line.replace("\n", "").split()
+        score += moveScore[f'{moves[0]}{moves[1]}']
+    print(score)
 
 
 
