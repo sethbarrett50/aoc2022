813fd63110c69bb4b34ff4d4390848ff48b85d71
Seth <sethbarrett01@gmail.com>
2022-12-04 09:27:55 -0500

    Day 4, Parts 1 && 2 done

--- day4/pt1.py
+++ day4/pt1.py
@@ -1,12 +1,22 @@
 # Day 4 AoC 2022
 # Part 1
+import re
 
-
-
+# We love Python sets
 
 def main():
+    pattern, sets = re.compile(r"([\d]*)-([\d]*),([\d]*)-([\d]*)"), []
+
     with open('input.txt') as f:
         lines = f.readlines()
+
+    for line in lines:
+        match = pattern.match(line)
+        start1, end1, start2, end2 = int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4))
+        sets.append((set(range(start1, end1 + 1)), set(range(start2, end2 + 1))))
+    
+    print(sum(one >= two or two >= one for one, two in sets))
+
 
         
 
--- day4/pt2.py
+++ day4/pt2.py
@@ -1,14 +1,25 @@
 # Day 4 AoC 2022
 # Part 2
+import re
 
-
+# We love Python sets
 
 
 def main():
+    pattern, sets, num = re.compile(r"([\d]*)-([\d]*),([\d]*)-([\d]*)"), [], 0
+
     with open('input.txt') as f:
         lines = f.readlines()
 
-
+    for line in lines:
+        match = pattern.match(line)
+        start1, end1, start2, end2 = int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4))
+        sets.append((set(range(start1, end1 + 1)), set(range(start2, end2 + 1))))
+    
+    for one, two in sets:
+        if one&two:
+            num += 1
+    print(num)
 
 
 if __name__ == '__main__':
