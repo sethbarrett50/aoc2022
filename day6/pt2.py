d9b9cb469750517d8db722b58e594450a4083d6e
Seth <sethbarrett01@gmail.com>
2022-12-05 01:07:59 -0500

    Day 6 prep completed

---
+++ day6/pt2.py
@@ -0,0 +1,16 @@
+# Day 6 AoC 2022
+# Part 2
+
+import collections, re, pdb
+
+def main():
+    
+    with open('input.txt') as f:
+        lines = f.readlines()
+
+        
+
+
+
+if __name__ == '__main__':
+    main()
