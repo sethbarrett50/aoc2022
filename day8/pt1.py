9fac872f51bc4c06b5b77795a6417a18ffbb82fc
Seth <sethbarrett01@gmail.com>
2022-12-07 10:20:44 -0500

    Day 8 prep done

---
+++ day8/pt1.py
@@ -0,0 +1,18 @@
+# Day 8 AoC 2022
+# Part 1
+
+def main():
+    
+    with open('input.txt') as f:
+        lines = f.readlines()
+
+            
+
+
+
+        
+
+
+
+if __name__ == '__main__':
+    main()
